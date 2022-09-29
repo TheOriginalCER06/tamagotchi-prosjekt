496db12bd9bbf3d45c8880c51edaa1bc5ecdb3c6
TheOriginalCER06 <69149072+TheOriginalCER06@users.noreply.github.com>
2022-09-29 08:24:35 +0200

    Test of uploading

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
+def on_button_pressed_a():
+    global isAwake, meny
+    if isAwake == False:
+        isAwake = True
+        stopScreenActionsAndClear()
+    else:
+        meny += 1
+        stopScreenActionsAndClear()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def stopScreenActionsAndClear():
+    led.stop_animation()
+    basic.clear_screen()
 isAwake = False
+laget_av = "- Christian E. Røren"
+isAwake = False
+meny = 0
 
 def on_every_interval():
-    global isAwake
-    isAwake = False
-loops.every_interval(1000, on_every_interval)
+    pass
+loops.every_interval(10000, on_every_interval)
 
 def on_forever():
-    global isAwake
-    isAwake = True
-    basic.show_string("" + str((isAwake)))
+    global meny, isAwake
+    meny = meny % 2
+    if meny == 0:
+        if isAwake == True:
+            if True:
+                basic.show_icon(IconNames.HAPPY)
+        else:
+            basic.show_string("ZzZzZzZz")
+    elif meny == 1:
+        isAwake = True
+        basic.show_icon(IconNames.HEART)
+    else:
+        pass
 basic.forever(on_forever)
