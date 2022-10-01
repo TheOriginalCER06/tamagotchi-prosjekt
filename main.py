58d8d59113ad07e3d180fc65859c4bd5fdea4c42
TheOriginalCER06 <69149072+TheOriginalCER06@users.noreply.github.com>
2022-10-01 19:35:41 +0200

    Prøvde å lage Barer og endret småting

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def on_button_pressed_a():
-    global isAwake, meny
+    global isAwake, våkenhet, glede, meny
     if isAwake == False:
         isAwake = True
         stopScreenActionsAndClear()
+        våkenhet = 5
+        glede += 1
     else:
         meny += 1
         stopScreenActionsAndClear()
@@ -11,17 +13,29 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 def stopScreenActionsAndClear():
     led.stop_animation()
     basic.clear_screen()
+våkenhet = 0
 isAwake = False
-laget_av = "- Christian E. Røren"
+laget_av = "- Christian E. Røren og Victor"
 isAwake = False
 meny = 0
+matNivå = 5
+våkenhet = 5
+glede = 5
+barerARRAY = [glede, våkenhet, matNivå]
 
 def on_every_interval():
-    pass
+    if True:
+        pass
 loops.every_interval(10000, on_every_interval)
 
 def on_forever():
     global meny, isAwake
+    for value in barerARRAY:
+        if barerARRAY[value] > 5:
+            barerARRAY[value] = 0
+        else:
+            pass
+    basic.show_string("" + str((glede)))
     meny = meny % 2
     if meny == 0:
         if isAwake == True:
--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ input.onButtonPressed(Button.A, function () {
     if (isAwake == false) {
         isAwake = true
         stopScreenActionsAndClear()
+        våkenhet = 5
+        glede += 1
     } else {
         meny += 1
         stopScreenActionsAndClear()
@@ -11,14 +13,29 @@ function stopScreenActionsAndClear () {
     led.stopAnimation()
     basic.clearScreen()
 }
-let isAwake = false
-let laget_av = "- Christian E. Røren"
-isAwake = false
 let meny = 0
+let våkenhet = 0
+let isAwake = false
+let laget_av = "- Christian E. Røren og Victor"
+isAwake = false
+let matNivå = 5
+våkenhet = 5
+let glede = 5
+let barerARRAY = [glede, våkenhet, matNivå]
 loops.everyInterval(10000, function () {
-	
+    if (true) {
+    	
+    }
 })
 basic.forever(function () {
+    for (let value of barerARRAY) {
+        if (barerARRAY[value] > 5) {
+            barerARRAY[value] = 0
+        } else {
+        	
+        }
+    }
+    basic.showString("" + glede)
     meny = meny % 2
     if (meny == 0) {
         if (isAwake == true) {
